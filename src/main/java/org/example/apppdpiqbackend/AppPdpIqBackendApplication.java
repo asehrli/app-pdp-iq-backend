0d5903696dcfae771e04d0a8c8a66e92dfa4ddbc
farkh <farkhodovalisher@gmail.com>
2024-02-13 10:33:15 +0500

    initial

---
+++ src/main/java/org/example/apppdpiqbackend/AppPdpIqBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.apppdpiqbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppPdpIqBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppPdpIqBackendApplication.class, args);
+    }
+
+}
